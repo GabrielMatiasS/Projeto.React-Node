03054e613366d2917532df1a34ac2e7b7734997a
Gabriel Matias <gabrielmatiasdasilva9@gmail.com>
2022-09-11 01:01:15 -0300

    First Commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
-
+import NotificationButton from "./Components/NotificationButton"
 function App() {
   return(
+    <>
     <h1>hello world</h1>
+    <NotificationButton />
+    <NotificationButton />
+    <NotificationButton />
+    <NotificationButton />
+    </>
   )
 }
 
